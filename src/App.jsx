abe3e0851e97deaff2938eb30b041f253c3797a5
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-01-31 18:53:43 -0500

    Configuracion del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
-import { Plan } from './components/Plan'
+//import { Plan } from './components/Plan'
+import { Producto } from './components/Producto'
 
 function App() {
   
 
   return (
-    <div>
-      <Plan/>
+    <div className='container mx-auto mt-20'>
+      {/* <Plan/> */}
+      <Producto/>
     </div>
   )
 }
--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,7 +1,23 @@
 
 
 export const Producto = () => {
-  return (
-    <div>Producto</div>
-  )
+    return (
+        <div className="md:w-1/2 lg:w2/5 mx-5">
+            <h2>
+                25% de descuento
+            </h2>
+
+            <h2>
+                iPhone 12 Pro MaX Negro
+            </h2>
+
+            <p>
+                Hasta 12 cuotas de: {''} 
+                <span>$392.00</span>
+            </p>
+
+            <p>Precio Final: $1873.00</p>
+
+        </div>
+    )
 }
