166a5e5529f8f2b99e88b4a17c9aea0b38cf83d4
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-01-31 20:32:59 -0500

    Update Plan.jsx

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,37 +1,57 @@
-
+import { useState } from "react"
+import redes from '../assets/redes.png'
 
 export const Plan = () => {
+    const [gigas, setGigas] = useState('23')
+    const [precio, setPrecio] = useState('20.6')
+
     return (
-        <div className="md:w-1/2 lg:w2/5 mx-5">
-            <h2 className='font-black text-2xl text-center'>
+        <div className="max-w-sm p-8 bg-white border border-white-200 rounded-lg shadow  dark:border-white-800 text-center">
+            <h2 className='text-2xl text-center'>
                 Plan Movistar Bronze
             </h2>
 
-            <h1 className=" text-center"> 23 GB </h1>
+            <h1 className="text-2xl text-center font-bold"> {gigas} GB </h1>
 
-            <p>
+            <div className='container mx-auto mt-2 flex justify-center max-w-sm p-5 bg-gray-200 border border-gray-230 rounded-lg shadow  dark:border-gray-200 text-center'>
+                <h1>12 GB + 6 GB + 5 GB{''} 
+                    <span className="flex justify-center">
+                        <p>Navegación</p>
+                        <img src={redes} className="w-10" />
+                        <p> x 6 meses</p>
+                    </span>
+                </h1>
+
+                
+            </div>
+
+            <p className="container mx-auto mt-3 justify-center p-1 bg-blue-300  font-bold text-center text-white" >
                 Redes Sociales libres
             </p>
 
-            <h1>
+            <h1 className="mt-3">
                 $12.36 {''}
-                <span className='text-indigo-600 font-bold'>/mes</span>
+                <span className=' font-bold'>/mes</span>
             </h1>
 
-            <p>Precio Incluye IVA</p>
+            <p className="mt-1">Precio Incluye IVA</p>
 
-            <p>
+            <p className="mt-3 container mx-auto justify-center p-1 bg-green-400 rounded-full text-center font-bold text-green-700">
                 Recibe los beneficios del Plan {''}
                 <span>Superior x 6 meses</span>
             </p>
 
-            <p>Incluye 40% Dto. por 3 meses</p>
+            <p className="mt-3 container mx-auto justify-center p-1 bg-pink-200 rounded-full text-center font-bold text-pink-700">
+                Incluye 40% Dto. por 3 meses
+            </p>
 
-            <p>Precio al finalizar la promoción $20.6/mes</p>
+            <p className="mt-3">Precio al finalizar la promoción ${precio}/mes</p>
 
-            <button type="button" className='py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg'>
+            <button type="button" className='py-2 px-10 bg-blue-400  text-white font-bold uppercase rounded-full mt-3'>
                 Lo quiero!
             </button>
+
+            <div className=" mt-3 text-blue-300">Mas beneficios</div>
         </div>
     )
 }
